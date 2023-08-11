ae90cfe62408285463e89fd71263bb820491eff7
krutika <137177323+uygsj@users.noreply.github.com>
2023-08-11 15:27:06 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,15 @@
+
+
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Food Rs 10</div>
+        <div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+        </div>
+        </div>
+
+    );
+}
+export default ExpenseItem;
